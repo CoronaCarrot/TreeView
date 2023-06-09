9071f3cd973028705219b7afffec0108e7bd11f8
CarrotWillKillU <Carr0twillkillu@outlook.com>
2023-06-09 20:19:26 +0100

    changes

--- js/animation.js
+++ js/animation.js
@@ -3,3 +3,9 @@ window.addEventListener('scroll', () => {
     // set in :root too 
     document.documentElement.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
+
+// register back to top button
+var backToTop = document.getElementById("btt");
+backToTop.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
